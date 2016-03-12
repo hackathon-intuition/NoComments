3ee9f7334d977dbd9be389d0814a41bb877110ae
saidurga001 <saidurga001@e.ntu.edu.sg>
2016-03-13 01:28:08 +0800

    another random shit

---
+++ app/src/main/java/com/example/shrey_000/myapplication/AddProjectActivity.java
@@ -0,0 +1,70 @@
+package com.example.shrey_000.myapplication;
+
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ui.ParseLoginBuilder;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddProjectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_project);
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this);
+//        ParseLoginBuilder builder = new ParseLoginBuilder(AddProjectActivity.this);
+//        startActivityForResult(builder.build(), 0);
+//        testDbConnect();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    
+    public void testDbConnect() {
+        DbConnect dbConnect = new DbConnect(this);
+
+    }
+
+
+}
